02a4fe93f1eca915811a27e1893e59655297a519
NekrodNIK <NP17210@yandex.ru>
2022-03-13 16:00:35 +0500

    add settings open button in desktop version

---
+++ lib/desktop/vertical_menu_bar.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class VerticalMenuBar extends StatelessWidget {
+  const VerticalMenuBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      const Spacer(),
+      Material(
+        color: Theme.of(context).colorScheme.secondary,
+        shape: ContinuousRectangleBorder(
+          borderRadius: BorderRadius.circular(28.0),
+        ),
+        child: IconButton(
+          onPressed: () {  },
+          color: Theme.of(context).colorScheme.onSecondary,
+          icon: const Icon(Icons.settings),
+          iconSize: 40,
+        ),
+      )
+    ]);
+  }
+}
