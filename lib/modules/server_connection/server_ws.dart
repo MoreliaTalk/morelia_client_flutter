f605b1c24f45957b536de17e94e428c8a0d3eb7b
NekrodNIK <NP17210@yandex.ru>
2022-03-22 20:44:19 +0500

    init websockets

---
+++ lib/modules/server_connection/server_ws.dart
@@ -0,0 +1,13 @@
+import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:web_socket_channel/status.dart' as status;
+
+class ServerWebsockets {
+  final String _url;
+  late WebSocketChannel _channel;
+
+  ServerWebsockets(this._url);
+
+  void connect() {
+    _channel = WebSocketChannel.connect(Uri.parse(_url));
+  }
+}
