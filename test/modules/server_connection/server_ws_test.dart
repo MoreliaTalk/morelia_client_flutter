7ed6856b1f131c1a0dcb438d67e3ff7d84570da7
Nekrod <NP17210@yandex.ru>
2022-03-28 18:32:15 +0500

    server_ws_test.dart init

---
+++ test/modules/server_connection/server_ws_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:morelia_client_flutter/modules/server_connection/server_ws.dart';
+
+void main() {
+  test("Test register_user", () {
+
+  });
+}
