933ba145eea0c0ce5e5afdec62d185806538b0d2
Nekrod <NP17210@yandex.ru>
2022-03-31 16:18:55 +0500

    add test for all_flow

--- test/modules/server_connection/server_ws_test.dart
+++ test/modules/server_connection/server_ws_test.dart
@@ -233,6 +233,33 @@ void main() {
           flow_info: "flow_info");
       await completer.future;
     });
+
+    test("Test all_flow", () async {
+      final server = await io.HttpServer.bind("localhost", 8444);
+
+      Completer completer = Completer();
+      server.transform(io.WebSocketTransformer()).listen((webSocket) {
+        webSocket.listen((request) {
+          var newRequest = api.Validator(type: "all_flow");
+
+          newRequest.jsonapi = api.Version(
+              version: api.protocolVersion, revision: api.protocolRevision);
+
+          newRequest.data = api.Data();
+
+          newRequest.data?.user = [];
+          newRequest.data?.user?.add(api.User(auth_id: "auth_id", uuid: "user_uuid"));
+          assert(request == jsonEncode(newRequest.toJson()));
+          completer.complete();
+        });
+      });
+
+      var connection = ServerConnection("ws://localhost:8444/");
+      connection.connect();
+
+      connection.all_flow(user_uuid: "user_uuid", auth_id: "auth_id");
+      await completer.future;
+    });
   });
 }
 
