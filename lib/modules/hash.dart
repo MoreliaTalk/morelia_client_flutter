31f02fb50d9568a43588b8c5fa59e821b8a66df3
Stepan Skriabin <stepan.skrjabin@gmail.com>
2022-04-24 20:55:13 +0300

    refactor(hash): refactoring and added docstring

--- lib/modules/hash.dart
+++ lib/modules/hash.dart
@@ -18,35 +18,47 @@ import 'dart:math';
 
 import 'package:cryptography/cryptography.dart';
 
-Future<Map<String, String>> hashPassword(String password) async {
+/// Object contains hash from password and generated salt and key.
+class HashPasswordObject {
+  String hash;
+  String? salt;
+  String? key;
+
+  HashPasswordObject(this.hash, this.salt, this.key);
+}
+
+/// Generate hash for password.
+///
+/// Returns [HashPasswordObject] which contains hash from password salt and key.
+///
+/// [salt] and [key] optional parameters and can be generated within the function.
+Future<HashPasswordObject> hashPassword(String password,
+    {String? salt, String? key}) async {
   final algorithm = Blake2b();
   const decoder = Utf8Decoder(allowMalformed: true);
   String input;
-  final salt = genString(16);
-  final key = genString(8);
 
+  // check if salt or key is null
+  // if null, then we generate ascii string
+  salt = salt ?? genString(16);
+  key = key ?? genString(8);
   input = password + salt + key;
 
   final hash = await algorithm.hash(input.runes.toList());
-  final Map<String, String> result;
 
-  result = {
-    "hashPassword": decoder.convert(hash.bytes),
-    "salt": salt,
-    "key": key
-  };
-
-  return result;
+  return HashPasswordObject(decoder.convert(hash.bytes), salt, key);
 }
 
+/// Generated fixed length string with random ASCII symbol and numbers.
+/// [stringLength] length in symbols.
 String genString(int stringLength) {
-  const _dictionary =
-      "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890";
+  const _symbols = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+  const _numbers = "1234567890";
+  const _specSymbols = "!@#%^&*(){}[];:<>,.?`~-+=_";
+  const _dictionary = _symbols + _numbers + _specSymbols;
   const maxNumber = _dictionary.length;
-  Random _rnd = Random();
-  var result;
+  final Random _rnd = Random();
 
-  result = String.fromCharCodes(Iterable.generate(stringLength,
+  return String.fromCharCodes(Iterable.generate(stringLength,
       (index) => _dictionary.codeUnitAt(_rnd.nextInt(maxNumber))));
-  return result;
 }
