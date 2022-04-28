645b762fca6d4a7b313fbf1fef65e3e147fe28ce
Stepan Skriabin <stepan.skrjabin@gmail.com>
2022-04-28 18:07:43 +0300

    feat(db_initialize): added Copyright

--- lib/modules/database/db_initialize.dart
+++ lib/modules/database/db_initialize.dart
@@ -1,3 +1,18 @@
+// Copyright (c) 2022 - present Stepan Skriabin.
+// Look at the file AUTHORS.md (located at the root of the project) to get the
+// full list.
+// This file is part of Morelia Flutter.
+// Morelia Flutter is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// Morelia Flutter is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with Morelia Flutter. If not, see <https://www.gnu.org/licenses/>.
+
 import 'dart:io';
 
 import 'package:isar/isar.dart';
