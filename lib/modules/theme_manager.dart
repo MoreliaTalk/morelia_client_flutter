c3ab75c5e05bc3d5bf72f6adad51311c1a761ee7
nekrod <NP17210@yandex.ru>
2022-05-07 17:06:27 +0500

    fix

--- lib/modules/theme_manager.dart
+++ lib/modules/theme_manager.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final lightTheme = ThemeData.light()
+final lightTheme = ThemeData.light();
 final darkTheme = ThemeData.dark();
 
 final currentTheme = StateProvider<ThemeData>((ref) => darkTheme);
