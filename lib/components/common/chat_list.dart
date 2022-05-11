f7850a59369320286d1b94d038c118ff805c7075
nekrod <NP17210@yandex.ru>
2022-05-11 22:16:52 +0500

    fix imports

--- lib/components/common/chat_list.dart
+++ lib/components/common/chat_list.dart
@@ -1,5 +1,4 @@
 import 'package:faker/faker.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:morelia_client_flutter/modules/database/db.dart';
